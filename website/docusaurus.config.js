d799239031139627260eda66d76ffc4ed66dbb94
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-01 15:59:03 +0300

    squash!

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -152,7 +152,7 @@ module.exports = {
         theme: {
           customCss: [
             require.resolve('./src/css/prism-rose-pine-moon.css'),
-            require.resolve('./src/css/custom.css')
+            require.resolve('./src/css/Custom.css')
           ],
         },
       },
