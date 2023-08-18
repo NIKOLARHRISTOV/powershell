7a23af8e421d9cb962f4d32a580ba267f04538f5
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-18 14:57:02 +0300

    squash!

--- src/segments/quasar_test.go
+++ src/segments/quasar_test.go
@@ -14,7 +14,7 @@ import (
 func TestQuasar(t *testing.T) {
 	packageLockFile := `{
 		"name": "quasar-project",
-		"version": "0.0.1",
+		"version": "0.0.2",
 		"lockfileVersion": 2,
 		"requires": true,
 		"dependencies": {
